c23c709cf8d7715a68abf3d0a68903904e039491
benie254 <davinci.monalissa@gmail.com>
2023-09-01 16:55:11 +0300

    initialize a box challenge

--- meme/src/App.tsx
+++ meme/src/App.tsx
@@ -1,11 +1,34 @@
-import Meme from "./components/Meme"
-import Navbar from "./components/Navbar"
+import { useState } from "react";
+import Meme from "./components/Meme";
+import Navbar from "./components/Navbar";
+import Boxes from "./data/Boxes";
+
+interface BoxesInter {
+  id: number;
+  on: boolean;
+}
 
 function App() {
+
+  let boxArray = Boxes;
+  const [boxes, setBoxes] = useState<BoxesInter[]>(boxArray);
+
+  const allBoxes = boxes.map(
+    (box) => (
+        <div>
+          <div className="boxes" key={box.id}></div> <br />
+        </div>
+    )
+  )
+
   return (
     <>
-      <Navbar />
-      <Meme />
+      {/* <Navbar />
+      <Meme /> */}
+      <main>
+        <h1>Boxes</h1>
+        {allBoxes}
+      </main>
     </>
   )
 }
