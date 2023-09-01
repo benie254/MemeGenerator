b94778eb795279c57784b9507a3a27b49d839e8f
benie254 <davinci.monalissa@gmail.com>
2023-09-01 16:37:43 +0300

    remove logic for passing state from parent to children components

--- meme/src/App.tsx
+++ meme/src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from "react"
-import Body from "./components/Body"
-import Header from "./components/Header"
 import Meme from "./components/Meme"
 import Navbar from "./components/Navbar"
 
 function App() {
-  const [user, setUser] = useState<string>("Joe");
   return (
     <>
-      {/* <Navbar />
-      <Meme /> */}
-      <Header user={user} />
-      <Body user={user} />
+      <Navbar />
+      <Meme />
     </>
   )
 }
