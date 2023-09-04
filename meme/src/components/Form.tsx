e1311f9f0c116d84cde2a404ea01bd23650d93ee
benie254 <davinci.monalissa@gmail.com>
2023-09-04 12:18:15 +0300

    convert uncontrolled inputs to controlled with the value attribute

--- meme/src/components/Form.tsx
+++ meme/src/components/Form.tsx
@@ -21,11 +21,29 @@ export default function Form() {
         <>
         <form action="">
             <h4>first Name: {formData.firstName}</h4>
-            <input type="text" placeholder="First name..." name="firstName" onChange={handleChange} />
+            <input 
+                type="text" 
+                placeholder="First name..." 
+                name="firstName" 
+                onChange={handleChange} 
+                value={formData.firstName}
+            />
             <h4>last Name: {formData.lastName}</h4>
-            <input type="text" placeholder="Last name..." name="lastName" onChange={handleChange} />
+            <input 
+                type="text" 
+                placeholder="Last name..." 
+                name="lastName" 
+                onChange={handleChange} 
+                value={formData.lastName}
+            />
             <h4>email: {formData.email}</h4>
-            <input type="email" placeholder="Email address..." name="email" onChange={handleChange} />
+            <input 
+                type="email" 
+                placeholder="Email address..." 
+                name="email" 
+                onChange={handleChange} 
+                value={formData.email}
+            />
         </form>
         </>
     )
