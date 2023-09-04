22e7e7e3293cf6148e0482cdd9d8a78c21b87b6f
benie254 <davinci.monalissa@gmail.com>
2023-09-04 16:40:00 +0300

    attempt making api fetch requests with use depenndencies

--- meme/src/components/Effects.tsx
+++ meme/src/components/Effects.tsx
@@ -1,14 +1,39 @@
 import { useEffect, useState } from "react"
 
+interface StarWars {
+    id: number;
+    name: string;
+    height: string;
+    mass: string;
+    hair_color: string;
+    skin_color: string;
+    eye_color: string;
+    birth_year: string;
+    gender: string;
+    homeworld: string;
+    films: string[];
+    species: [];
+    vehicles: string[];
+    starships: string[];
+    created: string;
+    edited: string;
+    url: string;
+}
+
 export default function() {
     const [count, setCount] = useState<number>(0);
-
+    const [swData, setSwData] = useState<StarWars[]>([]);
+ 
     function add(){
         setCount(prevCount => prevCount + 1);
     }
 
     useEffect(() => {
-            console.log("effect function ran");
+            fetch(`https://swapi.dev/api/people/${count}`).then(
+                res => res.json()
+            ).then(
+                data => setSwData(data)
+            );
         },[count]);
 
     return (
@@ -16,8 +41,12 @@ export default function() {
             <div>
                 <h2>The count is {count}</h2>
                 <button onClick={add}>
-                    Add
+                    Get Next Character
                 </button>
+                {
+                    swData && 
+                    <pre>{JSON.stringify(swData, null, 2)}</pre>
+                }
             </div>
         </>
     )
